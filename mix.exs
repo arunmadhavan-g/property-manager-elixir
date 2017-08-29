f0e0b2c8f898a822012fd6bd941427b264ab018c
arunmadhavan-g <arunmadhavan.g@gmail.com>
2017-08-29 22:15:04 +0530

    Added coveralls for code coverage reporting

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule PropertyManager.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
@@ -21,7 +23,8 @@ defmodule PropertyManager.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:redix, "~> 0.6.1"}
+      {:redix, "~> 0.6.1"},
+      {:excoveralls, "~> 0.7", only: :test}
     ]
   end
 end
